5c2a14160b76032425f8020d9c9799608eaf20b4
Adiyogi140211 <95163386+Adithya1402@users.noreply.github.com>
2022-11-19 15:17:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup (){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
